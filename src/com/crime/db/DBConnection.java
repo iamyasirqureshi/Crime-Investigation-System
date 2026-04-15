c9926b92941c817cfcd416ce7caa8d6190fa1352
iamyasirqureshi <yasirqureshi3158@gmail.com>
2026-04-16 03:03:09 +0530

    code changes

--- src/com/crime/db/DBConnection.java
+++ src/com/crime/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL      = "jdbc:mysql://localhost:3306/crime_db";
     private static final String USER     = "root";
-    private static final String PASSWORD = "YsrNasir$786"; // ← your MySQL password here
+    private static final String PASSWORD = "XXXXXXXXXXX"; // ← your MySQL password here
 
     static {
         try {
